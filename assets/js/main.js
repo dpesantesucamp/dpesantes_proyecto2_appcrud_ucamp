55747398844f23c6e18e987fc9621a876cbe0881
dpesantesucamp <108096553+dpesantesucamp@users.noreply.github.com>
2022-09-01 12:33:20 -0500

    Funcionalidad
    Se agrego y modifico el codigo JS y HTML para el CRUD , se trabajo con arreglos y LocalStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,86 @@
-const txtPelicula = document.getElementById('txtPelicula');
-const tblPeliculas = document.getElementById('tblPeliculas');
+const txtProducto = document.getElementById('txtProducto');
+const txtCantidad = document.getElementById('txtCantidad');
+const txtPrecio = document.getElementById('txtPrecio');
+const tblProductos = document.getElementById('tblProductos');
 
 
-let peliculas = (localStorage.getItem("peliculas")) ? JSON.parse(localStorage.getItem("peliculas")) : [];
+let productos =  (localStorage.getItem("productos")) ? JSON.parse(localStorage.getItem("productos")) : [];
+let cantidades = (localStorage.getItem("cantidades")) ? JSON.parse(localStorage.getItem("cantidades")) : [];
+let precios = (localStorage.getItem("precios")) ? JSON.parse(localStorage.getItem("precios")) : [];
 
-mostrarPeliculas();
+mostrarProductos();
 
 function guardar(){
     console.log("Entro a guardar");
-    const pelicula = txtPelicula.value;
-    peliculas.push(pelicula);
-    console.log('Escribio ${pelicula}');
-    console.log(peliculas);
+    const producto = txtProducto.value;
+    const cantidad = txtCantidad.value;
+    const precio = txtPrecio.value;
+    productos.push(producto);
+    cantidades.push(cantidad);
+    precios.push(precio);
+    console.log(productos);
+    console.log(cantidades);
+    console.log(precios);
     actualizarStorage();
-    mostrarPeliculas();
+    mostrarProductos();
     limpiarCajaTexto();
 }
 
 function limpiarCajaTexto()
 {
-    txtPelicula.value = "";
+    txtProducto.value = "";
+    txtCantidad.value = "";
+    txtPrecio.value = "";
 }
 
 function actualizarStorage()
 {
-    localStorage.setItem("peliculas",JSON.stringify(peliculas));
-    mostrarPeliculas();
+    localStorage.setItem("productos",JSON.stringify(productos));
+    localStorage.setItem("cantidades",JSON.stringify(cantidades));
+    localStorage.setItem("precios",JSON.stringify(precios));
+    mostrarProductos();
 }
 
-function mostrarPeliculas()
+function mostrarProductos()
 {
-    if (peliculas.length === 0)
+    if (productos.length === 0)
     {
-        tblPeliculas.innerHTML = `<tr class="text-center font-weight-bold">
-        <td colspan="2">NO HAY REGISTROS</td></tr>`;
+        tblProductos.innerHTML = `<tr class="text-center font-weight-bold">
+        <td colspan="4">NO HAY REGISTROS</td></tr>`;
     }else
     {
-        tblPeliculas.innerHTML = "";
-        for (const pelicula of peliculas)
+        tblProductos.innerHTML = "";
+        for (const producto of productos)
         {            
             const tr = document.createElement("tr");
-            const tdPelicula = document.createElement("td");
-            tdPelicula.innerText = pelicula;
-            tr.appendChild(tdPelicula);
-            tblPeliculas.appendChild(tr);
+            const tdProducto = document.createElement("td");
+            tdProducto.innerText = producto;
+            tr.appendChild(tdProducto);
+            tblProductos.appendChild(tr);
+
+            const index = productos.indexOf(producto);
+            const tdCantidad = document.createElement("td");
+            tdCantidad.innerText = cantidades[index];
+            tr.appendChild(tdCantidad);
+            tblProductos.appendChild(tr);
+
+            const tdPrecio = document.createElement("td");
+            tdPrecio.innerText = precios[index];
+            tr.appendChild(tdPrecio);
+            tblProductos.appendChild(tr);
 
             const tdAcciones = document.createElement("td");
             const btnEliminar = document.createElement("button");
             btnEliminar.innerText = "Eliminar";
             btnEliminar.classList.add("btn","btn-danger");
-            btnEliminar.onclick = () => eliminar(pelicula);
+            btnEliminar.onclick = () => eliminar(producto);
             tdAcciones.appendChild(btnEliminar);
             tr.appendChild(tdAcciones);
 
             const btnEditar = document.createElement("button");
             btnEditar.innerText = "Editar";
             btnEditar.classList.add("btn","btn-warning", "ml-2");
-            btnEditar.onclick = () => editar(pelicula);
+            btnEditar.onclick = () => editar(producto, cantidades[index], precios[index]);
             tdAcciones.appendChild(btnEditar);
             tr.appendChild(tdAcciones);
         }
@@ -64,18 +88,26 @@ function mostrarPeliculas()
     }
 }
 
-function eliminar(pelicula)
+function eliminar(producto)
 {
-        const index = peliculas.indexOf(pelicula);
-        peliculas.splice(index,1);
+        const index = productos.indexOf(producto);
+        productos.splice(index,1);
+        cantidades.splice(index,1);
+        precios.splice(index,1);
         actualizarStorage();
 }
 
-function editar(pelicula)
+function editar(producto, cantidad, precio)
 {
-        const index = peliculas.indexOf(pelicula);
-        const nuevo_nombre_pelicula = prompt(`Ingrese el nuevo nombre de la pelicula, 
-        actual: ${pelicula}`);
-        peliculas[index] = nuevo_nombre_pelicula;
+        const index = productos.indexOf(producto);
+        const nuevo_nombre_producto = prompt(`Ingrese el nuevo nombre del item, 
+        actual: ${producto}`);
+        productos[index] = nuevo_nombre_producto;
+        const nueva_cantidad = prompt(`Ingrese la nueva cantidad, 
+        actual: ${cantidad}`);
+        cantidades[index] = nueva_cantidad;
+        const nuevo_precio = prompt(`Ingrese el nuevo precio, 
+        actual: ${precio}`);
+        precios[index] = nuevo_precio;
         actualizarStorage();
 }
